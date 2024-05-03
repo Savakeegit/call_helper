83f9818f09ebcdaa1cd0c381a621f2c9e49655cb
Savakee <Savakee@gmail.com>
2024-05-03 20:55:53 +0300

    fix: added model break

--- breaks/admin.py
+++ breaks/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.admin import TabularInline
 
-from breaks.models import organisations, groups, replacements
+from breaks.models import organisations, groups, replacements, dicts, breaks
 
 
 ###############################
@@ -36,11 +36,27 @@ class ReplacementAdmin(admin.ModelAdmin):
     list_display = (
         'id', 'group', 'date', 'break_start', 'break_end', 'break_max_duration',
     )
-    inlines = (ReplacementEmployeeInline,)
+    inlines = (
+        ReplacementEmployeeInline,
+    )
 
 
-@admin.register(replacements.ReplacementStatus)
+@admin.register(dicts.ReplacementStatus)
 class ReplacementStatusAdmin(admin.ModelAdmin):
     list_display = (
         'code', 'name', 'sort', 'is_active',
+    )
+
+
+@admin.register(dicts.BreakStatus)
+class BreakStatusAdmin(admin.ModelAdmin):
+    list_display = (
+        'code', 'name', 'sort', 'is_active',
+    )
+
+
+@admin.register(breaks.Break)
+class BreakAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'replacement', 'break_start', 'break_end',
     )
